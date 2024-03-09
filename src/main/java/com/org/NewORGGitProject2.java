c203904569d5094e112a00844f7d08e3b6e37b8c
@ABSP884 <santhi.govardhan@gmail.com>
2024-03-09 07:33:51 +0200

    newscript2

---
+++ src/main/java/com/org/NewORGGitProject2.java
@@ -0,0 +1,22 @@
+package com.org;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewORGGitProject2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());		
+		driver.findElement(By.name("username")).sendKeys("santhi");
+		driver.findElement(By.name("password")).sendKeys("Santhi123");
+		System.out.println("second git project");
+	}
+
+}
